e93e9262f89a8e362ed404935e0b79f5b2b9edaa
dipelaez <diego.pelaez@hotmail.com.br>
2022-03-23 20:29:16 -0300

    revisited

--- main.c
+++ main.c
@@ -1,28 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dipelaez <diego.pelaez@hotmail.com.br>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/23 20:28:51 by dipelaez          #+#    #+#             */
+/*   Updated: 2022/03/23 20:28:53 by dipelaez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
 #include "ft_printf.h"
 #include	<stdio.h>
 
 int main(void)
 {
-    char chara = 'o';
-    char chare = 'a';
-    char charu = '\0';
-    int number = -3478214;
-    int nub    = 21345;
-    unsigned int a = 4294967295;
-    char *null_str = NULL;
-    char *p;
+    // char chara = 'o';
+    // char chare = 'a';
+    // char charu = '\0';
+    // int number = -3478214;
+    // int nub    = 21345;
+    // unsigned int a = 4294967295;
+    // char *null_str = NULL;
+    // char *p;
     
     
-    ft_printf("%c qualquer, %s teste %c novo %c%c\n\n", chara, "bem marromenos dessa vez", chare, charu, charu);
-    ft_printf("Int negativo = %d\n", number);
-    ft_printf("Int positivo = %d\n", nub);
-    ft_printf("Unsigned int = %u\n", a);
-    ft_printf("Imprimindo porcentagem%%%%% asdasda\n%");
-    ft_printf("Imprimindo x = %x\n", 26);
-    ft_printf("Imprimindo X = %X\n", 26);
-    ft_printf("%s\n", null_str);
-    ft_printf("%p endereço bugado\n", &p);
-	ft_printf("%s", "");
+    // ft_printf("%c qualquer, %s teste %c novo %c%c\n\n", chara, "bem marromenos dessa vez", chare, charu, charu);
+    // ft_printf("Int negativo = %d\n", number);
+    // ft_printf("Int positivo = %d\n", nub);
+    // ft_printf("Unsigned int = %u\n", a);
+    // ft_printf("Imprimindo porcentagem%%%%% asdasda\n%");
+    // ft_printf("Imprimindo x = %x\n", 26);
+    // ft_printf("Imprimindo X = %X\n", 26);
+    // ft_printf("%s\n", null_str);
+    // ft_printf("%p endereço bugado\n", &p);
+	// ft_printf("%s", "");
+    // ft_printf(" %x .", 9223372036854775807LL);
+    // puts("");
+    printf(" %x ", 9223372036854775807LL);
    
     return (0);
 }
